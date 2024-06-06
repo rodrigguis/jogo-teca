878caf4048a2a1e76a7ddc2d67ae1a5e3775f763
rodrigguis <rodrigguis@gmail.com>
2024-06-06 18:21:41 -0300

    feat: reestruturacao app

---
+++ config.py
@@ -0,0 +1,13 @@
+""" Modulo Config """
+
+SECRET_KEY = 'alura'
+
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'admin',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
